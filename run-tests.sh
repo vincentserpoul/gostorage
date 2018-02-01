a858f70f9d5fe26ff2ea2d892ea8558d955e7ffd
Vincent Serpoul <vincent@serpoul.com>
2018-02-01 11:56:36 +0800

    use flag exclude instead of vendor

--- run-tests.sh
+++ run-tests.sh
@@ -3,15 +3,8 @@
 # any subsequent error will exit
 set -e
 
-# don't use dupl and safesql for CI
-gometalinter --disable-all --deadline 5m --vendor ./... \
- --enable=deadcode --enable=errcheck --enable=gas \
- --enable=goconst --enable=gocyclo --enable=goimports --enable=golint \
- --enable=gosimple --enable=gotype --enable=gotypex --enable=ineffassign \
- --enable=interfacer --enable=lll --enable=maligned --enable=megacheck \
- --enable=misspell --enable=nakedret --enable=staticcheck \
- --enable=structcheck --enable=unconvert --enable=unparam --enable=unused \
- --enable=varcheck --enable=vet
+# --vendor flag doesn't seem to work
+gometalinter --exclude=vendor --deadline 5m  ./...
 
 docker stack deploy --compose-file=infra/compose.yml storage;
 
