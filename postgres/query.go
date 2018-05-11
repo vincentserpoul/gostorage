d4389bad5a5c78768407404317128b63b00b6479
Vincent Serpoul <vincent@serpoul.com>
2018-05-11 16:25:36 +0800

    new interface for postgres

--- postgres/query.go
+++ postgres/query.go
@@ -2,23 +2,15 @@ package postgres
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 
 	"github.com/jmoiron/sqlx"
 )
 
-// SQLNamedExecuter is an interface used for CUD
-type SQLNamedExecuter interface {
-	NamedExecContext(ctx context.Context, query string, arg interface{}) (
-		sql.Result, error,
-	)
-}
-
-// QueryFilter allows for select filters
-type QueryFilter struct {
-	FilterSQL   string
-	NamedParams map[string]interface{}
+// SQLPrepareNamed is THE most useful interface
+// as postgres requires two phase to insert, with a prepare stmt
+type SQLPrepareNamed interface {
+	PrepareNamed(query string) (*sqlx.NamedStmt, error)
 }
 
 // SQLNamedQueryer is used to do select queries
@@ -26,6 +18,12 @@ type SQLNamedQueryer interface {
 	NamedQueryContext(ctx context.Context, query string, arg interface{}) (
 		*sqlx.Rows, error,
 	)
+}
+
+// QueryFilter allows for select filters
+type QueryFilter struct {
+	FilterSQL   string
+	NamedParams map[string]interface{}
 }
 
 // ErrSQLNotFound happens when no rows were found
