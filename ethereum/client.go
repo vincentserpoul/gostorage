efed0229fd2070c741bec117c8eba034b6e83c2a
Vincent Serpoul <vincent@serpoul.com>
2019-02-11 18:09:41 +0800

    add ws client for eth

--- ethereum/client.go
+++ ethereum/client.go
@@ -9,17 +9,20 @@ import (
 
 // Config is a conf for the ethereum node
 type Config struct {
-	Protocol    string
-	Host        string
-	JSONRPCPort string
-	WSPort      string
-	NetworkID   int64
+	Host            string
+	JSONRPCPort     string
+	JSONRPCProtocol string
+	WSProtocol      string
+	WSPort          string
+	NetworkID       int64
 }
 
 // NewRPCCli connects to ethereum node and return a connection backend
 func NewRPCCli(ethConf Config) (*ethclient.Client, error) {
 	cli, err := ethclient.Dial(
-		ethConf.Protocol + "://" + ethConf.Host + ":" + ethConf.JSONRPCPort,
+		ethConf.JSONRPCProtocol +
+			"://" + ethConf.Host +
+			":" + ethConf.JSONRPCPort,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("NewRPCCli: rpc.Dial %v", err)
@@ -33,6 +36,33 @@ func NewRPCCli(ethConf Config) (*ethclient.Client, error) {
 		return nil,
 			fmt.Errorf(
 				"NewRPCCli: NetworkID %d "+
+					"different from wanted NetworkID %d",
+				networkID, ethConf.NetworkID,
+			)
+	}
+
+	return cli, nil
+}
+
+// NewWSCli connects to ethereum node and return a connection backend
+func NewWSCli(ethConf Config) (*ethclient.Client, error) {
+	cli, err := ethclient.Dial(
+		ethConf.WSProtocol +
+			"://" + ethConf.Host +
+			":" + ethConf.WSPort,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("NewWSCli: rpc.Dial %v", err)
+	}
+
+	networkID, errNID := cli.NetworkID(context.Background())
+	if errNID != nil {
+		return nil, fmt.Errorf("NewWSCli: NetworkID %v", errNID)
+	}
+	if networkID.Int64() != ethConf.NetworkID {
+		return nil,
+			fmt.Errorf(
+				"NewWSCli: NetworkID %d "+
 					"different from wanted NetworkID %d",
 				networkID, ethConf.NetworkID,
 			)
--- ethereum/client_test.go
+++ ethereum/client_test.go
@@ -12,38 +12,38 @@ func TestNewRPCCli(t *testing.T) {
 		{
 			name: "Working connection",
 			ethConf: Config{
-				Protocol:    "http",
-				Host:        "127.0.0.1",
-				JSONRPCPort: "8645",
-				NetworkID:   17,
+				JSONRPCProtocol: "http",
+				Host:            "127.0.0.1",
+				JSONRPCPort:     "8645",
+				NetworkID:       17,
 			},
 			wantErr: false,
 		},
 		{
 			name: "Working connection, wrong network",
 			ethConf: Config{
-				Protocol:    "http",
-				Host:        "127.0.0.1",
-				JSONRPCPort: "8645",
-				NetworkID:   15,
+				JSONRPCProtocol: "http",
+				Host:            "127.0.0.1",
+				JSONRPCPort:     "8645",
+				NetworkID:       15,
 			},
 			wantErr: true,
 		},
 		{
 			name: "Non working connection",
 			ethConf: Config{
-				Protocol:    "http",
-				Host:        "127.0.0.1",
-				JSONRPCPort: "8888",
+				JSONRPCProtocol: "http",
+				Host:            "127.0.0.1",
+				JSONRPCPort:     "8888",
 			},
 			wantErr: true,
 		},
 		{
 			name: "Non working Dial",
 			ethConf: Config{
-				Protocol:    "zxp",
-				Host:        "127.0.0.1",
-				JSONRPCPort: "8888",
+				JSONRPCProtocol: "zxp",
+				Host:            "127.0.0.1",
+				JSONRPCPort:     "8888",
 			},
 			wantErr: true,
 		},
@@ -55,6 +55,66 @@ func TestNewRPCCli(t *testing.T) {
 			if (err != nil) != tt.wantErr {
 				t.Errorf(
 					"NewRPCCli() error = %v, wantErr %v",
+					err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestNewWSCli(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		ethConf Config
+		wantErr bool
+	}{
+		{
+			name: "Working connection",
+			ethConf: Config{
+				WSProtocol: "ws",
+				Host:       "127.0.0.1",
+				WSPort:     "8646",
+				NetworkID:  17,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Working connection, wrong network",
+			ethConf: Config{
+				WSProtocol: "ws",
+				Host:       "127.0.0.1",
+				WSPort:     "8646",
+				NetworkID:  15,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Non working connection",
+			ethConf: Config{
+				WSProtocol: "ws",
+				Host:       "127.0.0.1",
+				WSPort:     "8888",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Non working Dial",
+			ethConf: Config{
+				WSProtocol: "zxp",
+				Host:       "127.0.0.1",
+				WSPort:     "8888",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewWSCli(tt.ethConf)
+			if (err != nil) != tt.wantErr {
+				t.Errorf(
+					"NewWSCli() error = %v, wantErr %v",
 					err, tt.wantErr)
 				return
 			}
